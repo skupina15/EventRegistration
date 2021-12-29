9fe8021ad0c5be16f35e4ede81ae6930ad9d6429
Gal petkovšek <gp1914@student.uni-lj.si>
2021-12-29 14:55:57 +0100

    openApi tested

--- api/src/main/java/si/fri/rso/skupina15/v1/EventRegistrationApplication.java
+++ api/src/main/java/si/fri/rso/skupina15/v1/EventRegistrationApplication.java
@@ -1,6 +1,6 @@
 package si.fri.rso.skupina15.v1;
 
-import com.kumuluz.ee.discovery.annotations.RegisterService;
+//import com.kumuluz.ee.discovery.annotations.RegisterService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Application;
 
 //@RegisterService
 @OpenAPIDefinition(info = @Info(title = "API for registrations", version = "v1", contact = @Contact(email = "rso@fri.uni-lj.si"), license = @
-        License(name = "dev"), description = "API for registrations."), servers = @Server(url ="http://localhost:8080/v1"))
+        License(name = "dev"), description = "API for registrations."), servers = @Server(url ="http://20.120.67.65:8080/registrations/v1"))
 @ApplicationPath("v1")
 public class EventRegistrationApplication extends Application {
 }
